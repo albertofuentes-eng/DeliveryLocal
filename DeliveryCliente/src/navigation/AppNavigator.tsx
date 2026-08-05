0951c2e7d2f2dfbb7f4e0c0ca77d6dc5a8084b94
Jose Alberto Fuentes <josefuentesdeleon603@gmail.com>
2026-08-05 11:45:36 -0600

    Login casi terminado

---
+++ DeliveryCliente/src/navigation/AppNavigator.tsx
@@ -0,0 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from "../screens/auth/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ DeliveryCliente/src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function LoginScreen() {
+  const [correo, setCorreo] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Delivery Local</Text>
+
+      <Text style={styles.subtitle}>
+        Tu comida favorita{"\n"}
+        hasta tu puerta
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <MaterialIcons
+          name="email"
+          size={22}
+          color="#777"
+        />
+
+        <TextInput
+          placeholder="Correo electrónico"
+          style={styles.input}
+          value={correo}
+          onChangeText={setCorreo}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <MaterialIcons
+          name="lock"
+          size={22}
+          color="#777"
+        />
+
+        <TextInput
+          placeholder="Contraseña"
+          secureTextEntry
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Iniciar sesión
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.register}>
+          Crear cuenta
+        </Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 25,
+    backgroundColor: "#ffffff",
+  },
+
+  title: {
+    fontSize: 34,
+    fontWeight: "bold",
+    color: "#E53935",
+    textAlign: "center",
+  },
+
+  subtitle: {
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 40,
+    color: "#666",
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
+    paddingHorizontal: 12,
+    marginBottom: 18,
+    height: 55,
+  },
+
+  input: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: "#E53935",
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  register: {
+    textAlign: "center",
+    marginTop: 25,
+    color: "#E53935",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+
+});
